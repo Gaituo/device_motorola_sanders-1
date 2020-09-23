68c3135be2a74353f85a6f348204c1a6ab3ccbd3
ashwatthama <sai404142@gmail.com>
2020-09-23 06:51:06 +0000

    sanders : beauty hax for logs
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,15 @@
 # vendor props for sanders
 #
 
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
