a07ad167d62bce379bac4305cf5223b55a35c4ba
ashwatthama <sai404142@gmail.com>
2021-01-19 00:40:42 +0000

    Revert "sanders : Build new aidl qti power HAL"
    This reverts commit ac7d2c7659d4aea5c899809db5f755921f1fce7f.

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.2-service \
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
